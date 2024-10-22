b68cf15331b1f06cffb5b55f098dfed7d51a3503
Damián Parrilli <125889767+damiparrilli@users.noreply.github.com>
2024-10-22 18:23:14 -0300

    TAOS added (Tailwind Animate on Scroll)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/preline/dist/*.js'],
+	content: {
+	  files: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/preline/dist/*.js'],
+	  relative: true,
+	  transform: (content) => content.replace(/taos:/g, ''),
+	},
 	theme: {
-		extend: {},
+	  extend: {},
 	},
 	plugins: [
-		require('preline/plugin'),
+	  require('preline/plugin'),
+	  require('taos/plugin'), // Plugin de TAOS agregado
 	],
-}
+	safelist: [
+	  '!duration-[0ms]',
+	  '!delay-[0ms]',
+	  'html.js :where([class*="taos:"]:not(.taos-init))', // Safelist de TAOS
+	],
+  }
+  
